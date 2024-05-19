fba1d7e61ab5bd0ca15618c58048da60a63d160d
Gurvir Singh <gurvirsingh.baraich@gmail.com>
2024-05-19 10:13:47 +0530

    v0.0.22

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,3 @@
 <?php
 
 use Nest\Framework\Http\Route;
-
-class HomeController
-{
-  public function index()
-  {
-  }
-
-  public function path()
-  {
-  }
-}
-
-Route::GET("/:category/:categoryId", [HomeController::class, "path"]);
-Route::GET("/", [HomeController::class, "index"]);
